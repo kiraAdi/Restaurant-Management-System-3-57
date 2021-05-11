727ea04ed6d54e716bd603cc837bf3b154662c9d
kiraAdi <81691683+kiraAdi@users.noreply.github.com>
2021-05-12 00:52:44 +0530

    first commit

---
+++ implementation/Customer/sitting_arrangement.cpp
@@ -0,0 +1,15 @@
+#include "sitting_arrangement.h"
+//#include "ui_seating_arrangement.ui"
+
+SittingArrangement::SittingArrangement(QWidget *parent)
+    /*QMainWindow(parent),
+    ui(new Ui::SittingArrangement)*/
+{
+   // ui->setupUi(this);
+}
+
+SittingArrangement::~SittingArrangement()
+{
+  //  delete ui;
+}
+
